85d8cf7b852b4d29b1dda8444bc8840645640a3a
wenyuan <y1967879624@163.com>
2025-11-18 21:22:08 +0800

    filter加入pipeline

---
+++ zrpc-core/src/main/java/cn/wenyuan/zrpc/core/filter/FilterManager.java
@@ -0,0 +1,37 @@
+package cn.wenyuan.zrpc.core.filter;
+
+import cn.wenyuan.zrpc.core.filter.impl.DefaultFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ServiceLoader;
+
+public class FilterManager {
+
+    private final List<Filter> filterList;
+
+    private static class Holder {
+        static final FilterManager INSTANCE = new FilterManager();
+    }
+
+    public static FilterManager getInstance(){
+        return Holder.INSTANCE;
+    }
+
+    private FilterManager(){
+        ServiceLoader<Filter> loader = ServiceLoader.load(Filter.class);
+        List<Filter> loadedFilter = new ArrayList<>();
+
+        for (Filter filter : loader) {
+            loadedFilter.add(filter);
+        }
+
+        this.filterList = Collections.unmodifiableList(loadedFilter);
+    }
+
+    public FilterChain buildChain() {
+        return new DefaultFilterChain(this.filterList);
+    }
+
+}
